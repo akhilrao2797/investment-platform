6a6d30a512f837726b8349c51b9142bc67d99f88
Akhil <akhilrao2797@gmail.com>
2021-10-11 21:46:33 +0530

    Changed configuration for service registry

--- cloud-gateway/src/main/java/com/invest/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/invest/cloud/gateway/CloudGatewayApplication.java
@@ -1,3 +1,5 @@
+package com.invest.cloud.gateway;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
