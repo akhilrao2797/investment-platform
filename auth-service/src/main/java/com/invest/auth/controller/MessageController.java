ac0843c2d54cd551ef366b095283f55e38f2fb6e
Akhil <akhilrao2797@gmail.com>
2021-10-17 16:38:32 +0530

    Controllers configured for auth-service

--- auth-service/src/main/java/com/invest/auth/controller/MessageController.java
+++ auth-service/src/main/java/com/invest/auth/controller/MessageController.java
@@ -1,9 +1,23 @@
 package com.invest.auth.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.invest.auth.service.TwilioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/message")
+@RequestMapping("/message/v1")
 public class MessageController {
+
+    @Autowired
+    TwilioService twilioService;
+
+    @GetMapping("/validate")
+    public void validateOtp(@RequestParam String otp, @RequestParam String user) throws Exception {
+        twilioService.validateOTP(otp, user);
+    }
+
+    @PostMapping("/send")
+    public void sendOtp(@RequestParam String user){
+        twilioService.sendOTP(user);
+    }
 }
