e70271d0fedd9a9f102def591f45c01f936efcb6
Akhil <akhilrao2797@gmail.com>
2021-10-19 21:30:53 +0530

    Subscription service - added new APIs

--- user-service/src/main/java/com/invest/user/controller/SubscriptionController.java
+++ user-service/src/main/java/com/invest/user/controller/SubscriptionController.java
@@ -1,13 +1,14 @@
 package com.invest.user.controller;
 
+import com.invest.user.model.Analyst;
 import com.invest.user.model.Subscription;
-import com.invest.user.repository.SubscriptionRepository;
 import com.invest.user.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/user-sub/v1")
@@ -16,16 +17,35 @@ public class SubscriptionController {
     @Autowired
     SubscriptionService subscriptionService;
 
-    @Autowired
-    SubscriptionRepository subscriptionRepository;
-
-    @GetMapping("/subscription('{subscriptionId}')")
+    @GetMapping("/subscription/{subscriptionId}")
     public ResponseEntity<Subscription> getSubscriptionDetails(@PathVariable long subscriptionId){
         return ResponseEntity.ok(subscriptionService.getSubscriptionInfo(subscriptionId));
     }
 
     @GetMapping("/subscription")
-    public ResponseEntity<List<Subscription>> getSubscriptionDetailsByUser(@RequestParam String userId){
-        return ResponseEntity.ok(subscriptionRepository.findByUserId(userId));
+    public ResponseEntity<Subscription> getSubscriptionDetailsByUser(@RequestParam String userId){
+        return ResponseEntity.ok(subscriptionService.getSubscriptionByTraderId(userId));
+    }
+
+    @PostMapping("/subscription")
+    public ResponseEntity<Subscription> addSubscription(@RequestParam String traderId,
+                                                        @RequestParam String analystId){
+        Subscription subscription = subscriptionService.addSubscription(traderId, analystId);
+        return ResponseEntity.ok(subscription);
+    }
+
+    @DeleteMapping("/subscription/{subscriptionId}")
+    public ResponseEntity deleteSubscription(@PathVariable long subscriptionId,
+                                                        @RequestParam String traderId,
+                                                        @RequestParam String analystId){
+        subscriptionService.deleteSubscription(subscriptionId, traderId, analystId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/subscription/{subscriptionId}/analysts")
+    public ResponseEntity<Set<Analyst>> getAnalystDetailsBySubscriptionId(@PathVariable long subscriptionId){
+        Subscription subscription = subscriptionService.getSubscriptionInfo(subscriptionId);
+        Set<Analyst> analysts = (subscription != null) ? subscription.getAnalystList() : new HashSet<>();
+        return ResponseEntity.ok(analysts);
     }
 }
